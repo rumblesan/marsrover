cd056414228bec2a5dd3343053b357db65d8295f
Guy John <guy@rumblesan.com>
2017-08-29 19:39:55 +0100

    Flesh out bot movement functions

--- src/Bot.hs
+++ src/Bot.hs
@@ -5,7 +5,7 @@ module Bot (
   commandBot
 ) where
 
-import Commands (Command)
+import Commands
 data Heading = North | East | South | West deriving (Eq, Show)
 
 type Coord = (Int, Int)
@@ -16,5 +16,25 @@ data Bot = Bot {
 } deriving (Eq, Show)
 
 commandBot :: Bot -> Command -> Bot
-commandBot = undefined
+commandBot bot command =
+  case command of TurnLeft -> bot { heading = turnLeft $ heading bot }
+                  TurnRight -> bot { heading = turnRight $ heading bot }
+                  MoveForward -> bot { position = moveForward (heading bot) (position bot) }
 
+turnLeft :: Heading -> Heading
+turnLeft North = West
+turnLeft East = North
+turnLeft South = East
+turnLeft West = South
+
+turnRight :: Heading -> Heading
+turnRight North = East
+turnRight East = South
+turnRight South = West
+turnRight West = North
+
+moveForward :: Heading -> Coord -> Coord
+moveForward North (x, y) = (x, y + 1)
+moveForward East (x, y) = (x + 1, y)
+moveForward South (x, y) = (x, y - 1)
+moveForward West (x, y) = (x - 1, y)
