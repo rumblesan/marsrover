275a2d41e889db4a1f462f677a774dd51efd53cb
Guy John <guy@rumblesan.com>
2017-08-30 23:23:31 +0100

    Allow for input and display of multiple missions

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,33 @@
 module Main where
 
+import Control.Monad (mapM_)
 import Control.Monad.State.Strict
+import qualified Data.List as L
 
+import Bot (Bot)
 import Planet (Planet)
 import qualified Planet
 
-import Explore (Mission(..), runMission, missionReport)
+import Explore (Mission(..), runMissions, missionReport)
 
 main :: IO ()
 main = do
   putStrLn "Let's explore Mars"
+  planet <- getPlanetData
+  missions <- getMissionData []
+  printMissionReports $ evalState (runMissions missions) planet
+
+getPlanetData :: IO Planet
+getPlanetData = do
   putStrLn "What's the width? (Single integer)"
   w <- read <$> getLine
   putStrLn "What's the height? (Single integer)"
   h <- read <$> getLine
+  return $ Planet.create w h
+
+getMissionData :: [Mission] -> IO [Mission]
+getMissionData prevMissions = do
+  putStrLn "\n**** New Mission"
   putStrLn "What's the Robots x starting position? (Single integer)"
   x <- read <$> getLine
   putStrLn "What's the Robots y starting position? (Single integer)"
@@ -22,10 +36,17 @@ main = do
   heading <- read <$> getLine
   putStrLn "What's the Robots command string? (Any combo of L, R and/or F)"
   commands <- getLine
-  let
-    planet = Planet.create w h
-    mission = Mission x y heading commands
-    result = evalState (runMission mission) planet
-  print $ missionReport result
+  let mission = Mission x y heading commands
+  putStrLn "Is there another Mission? (y/n)"
+  another <- getLine
+  if another == "y" then
+    getMissionData $ mission : prevMissions
+   else
+    return $ L.reverse $ mission : prevMissions
 
+printMissionReports :: [Either Bot Bot] -> IO ()
+printMissionReports reports =
+  do
+    putStrLn "\n************ Reports"
+    mapM_ (putStrLn . missionReport) reports
 
